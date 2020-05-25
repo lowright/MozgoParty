10e4a1f1f7d2d4cf2943710d31b9df0e4a8328ae
lowright <pandos1998@gmail.com>
2020-05-25 18:18:58 +0300

    For Preview V1

--- App.js
+++ App.js
@@ -1,36 +1,24 @@
 import React, {Component} from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import Video from 'react-native-video';
-
+import { WebView } from 'react-native-webview';
 export default class App extends Component{
 
   constructor(props) {
     super(props)
 
     this.state = {
-      index : 0,
-      data : [
-        {
+     index : 0,
+      data : { 
+        '0' : {
           id : '1',
           url : 'https://mozgo-qiuz-materials.s3.amazonaws.com/38716/doY3BYVSRHkVqqj6.mp4'
         },
-        {
-          id : '2',
-          url : 'https://mozgo-qiuz-materials.s3.amazonaws.com/38720/JghUEXzlOM4crZf1.mp4'
+        '1' : {
+          id : '1',
+          url : 'https://mozgo-qiuz-materials.s3.amazonaws.com/38716/doY3BYVSRHkVqqj6.mp4'
         },
-        {
-          id : '3',
-          url : 'https://mozgo-qiuz-materials.s3.amazonaws.com/38721/obGUBevqU8tA3rc6.mp4'
-        },
-        {
-          id : '5',
-          url : 'https://mozgo-qiuz-materials.s3.amazonaws.com/38722/CcZSoKHj4xwA1AS0.mp4'
-        },
-        {
-          id : '5',
-          url : 'https://mozgo-qiuz-materials.s3.amazonaws.com/38723/cHE6xKoWdjnbIzNQ.mp4'
-        }
-      ]
+      }
     }
   }
 
@@ -41,15 +29,15 @@ export default class App extends Component{
     const {data, index} = this.state
 
     return (
-      <Video
-        key={index}
-        source={{uri: `${data[index].url}`}}   
-        style={styles.backgroundVideo} 
-        onLoadStart={() => console.log('onLoadStart')}
-        onProgress={() => console.log('onProgress')}
-        onEnd={() => this.setState({index : index + 1})}
-      />
-      
+      // <Video
+      //   key={index}
+      //   source={{uri: `${data[index].url}`}}   
+      //   style={styles.backgroundVideo} 
+      //   onLoadStart={() => console.log('onLoadStart')}
+      //   onProgress={() => console.log('onProgress')}
+      //   onEnd={() => this.setState({index : index + 1})}
+      // />
+      <WebView source={{ uri: 'https://party-games.mozgo.com/MmlVdKQWab' }} />
     )
 
   }
