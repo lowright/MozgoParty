6e1c7d5341701717fb31ae689d4b3c074f4639b5
lowright <pandos1998@gmail.com>
2020-05-25 19:12:05 +0300

    FIx V1

--- app/components/GamesProtoBuf.js
+++ app/components/GamesProtoBuf.js
@@ -1,21 +1,19 @@
 import React from 'react'
 import {Text, View, StyleSheet } from 'react-native'
-
-export const BottomQuestionNavigation = ({title}) => {
+import { WebView } from 'react-native-webview';
+export const GamesProtoBuf = ({jsonGames}) => {
     return(
-        <View style={styles.container}>
-            <Text>{title}</Text>
-        </View>
+        <WebView source={{ uri: `${jsonGames}` }} />
     )
 }
 
-export default BottomQuestionNavigation
+export default GamesProtoBuf
 
 const styles = StyleSheet.create({
     container : {
         flex : 1,
         justifyContent : 'space-between',
-        
+
         flexDirection : 'row',
         width : '100%',
         paddingHorizontal : 30
