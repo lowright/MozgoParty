8bdeefff4fe93200c90110045b0ac1533c42589d
itallianez <itallianez96@gmail.com>
2020-06-09 00:10:03 +0300

    'com'

--- app/screens/Profile/ChangePass.js
+++ app/screens/Profile/ChangePass.js
@@ -1,43 +1,85 @@
 import React from "react";
 import {View, Text, StyleSheet, TouchableOpacity, TextInput} from "react-native";
 
-export const ChangePass = props => {
-    return(
-        <View style={styles.ChangePassWrapper}>
-            <View style={styles.ChangePassInputWrapper}>
-                <Text style={styles.ChangePassDecorText}>Текущий пароль</Text>
-                <TextInput
-                    style={styles.PassInput}
-                    secureTextEntry={true}
-                    value={{}}
-                />
+export class ChangePass extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            stateStyle: 'styles.ChangePassDecorText',
+            active: 'ChangePassDecorTextActive',
+            actives:false,
+            oldPass:'',
+            newPass:'',
+            repeatPass:'',
+            oldPass1:'Текущий пароль',
+            newPass1:'Новый пароль',
+            repeatPass1:'Повторите пароль'
+        }
+    }
+    startEdit(val, id){
+        console.log('соси')
+        if(this.state.actives){
+            return val
+        } return ''
+    }
+    endEdit(val){
+        if(val.length <= 0){
+            return this.setState({actives:false})
+        } return ''
+    }
+    render(){
+        return(
+            <View style={styles.ChangePassWrapper}>
+                <View style={styles.ChangePassInputWrapper}>
+                    <Text style={styles.ChangePassDecorTextActive}>{this.startEdit(this.state.oldPass1, 1)}</Text>
+                    <TextInput
+                        style={styles.PassInput}
+                        secureTextEntry={true}
+                        value={this.state.oldPass}
+                        onChangeText={text => this.setState({oldPass : text})}
+                        placeholder={this.state.oldPass1}
+                        onEndEditing={()=>this.endEdit(this.state.oldPass)}
+                        onFocus={()=> this.setState({actives:true})}
+
+                    />
+                </View>
+                <View style={styles.ChangePassInputWrapper}>
+                    <Text style={styles.ChangePassDecorTextActive}>{this.startEdit(this.state.newPass1, 2)}</Text>
+                    <TextInput
+                        style={styles.PassInput}
+                        secureTextEntry={true}
+                        value={this.state.newPass}
+                        onChangeText={text => this.setState({newPass : text})}
+                        placeholder={this.state.newPass1}
+                        onEndEditing={()=>this.endEdit(this.state.newPass)}
+                        onFocus={()=> this.setState({actives:true})}
+
+                    />
+                </View>
+                <View style={styles.ChangePassInputWrapper}>
+                    <Text style={styles.ChangePassDecorTextActive}>{this.startEdit(this.state.repeatPass1, 3)}</Text>
+                    <TextInput
+                        style={styles.PassInput}
+                        secureTextEntry={true}
+                        value={this.state.repeatPass}
+                        onChangeText={text => this.setState({repeatPass : text})}
+                        placeholder={this.state.repeatPass1}
+                        onEndEditing={()=>this.endEdit(this.state.repeatPass)}
+                        onFocus={()=> this.setState({actives:true})}
+
+                    />
+                </View>
+                <View style={styles.ForgotPassWrapper}>
+                    <TouchableOpacity
+                        onPress={()=>{}}
+                        activeOpacity={0.9}
+                    >
+                        <Text style={styles.ForgotPassText}>Забыли пароль?</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
-            <View style={styles.ChangePassInputWrapper}>
-                <Text style={styles.ChangePassDecorText}>Новый пароль</Text>
-                <TextInput
-                    style={styles.PassInput}
-                    secureTextEntry={true}
-                    value={{}}
-                />
-            </View>
-            <View style={styles.ChangePassInputWrapper}>
-                <Text style={styles.ChangePassDecorText}>Повторите пароль</Text>
-                <TextInput
-                    style={styles.PassInput}
-                    secureTextEntry={true}
-                    value={{}}
-                />
-            </View>
-            <View style={styles.ForgotPassWrapper}>
-                <TouchableOpacity
-                    onPress={()=>{}}
-                    activeOpacity={0.9}
-                >
-                    <Text style={styles.ForgotPassText}>Забыли пароль?</Text>
-                </TouchableOpacity>
-            </View>
-        </View>
-    )
+        )
+    }
 }
 
 const styles = StyleSheet.create({
